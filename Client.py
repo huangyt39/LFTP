8eed7874c1c052169b2a0479970596a9d2dd2d29
huangyt39 <824551061@qq.com>
2018-11-13 22:20:26 +0800

    add Ack Seq

--- Client.py
+++ Client.py
@@ -1,46 +1,46 @@
 # -*- coding:utf-8 -*-
 
+import time
 from socket import *
 import uu
+import pdb
 
 HOST = '127.0.0.1'
-PORT = 21567
-BUFSIZ = 100024
+PORT = 21566
+BUFSIZ = 1024
 ADDR = (HOST, PORT)
+DISADDR = (HOST, 21567)
 
 udpCliSock = socket(AF_INET, SOCK_DGRAM)
+udpCliSock.bind(ADDR)
 
-uu.encode('test.mp4', 'test.txt')
-f = open('test.txt', 'r')
+# uu.encode('test.mp4', 'test.txt')
+f = open('D:/gitRep/LTFP/test.mp4', 'rb')
 data = f.read()
 
 size = len(data)
-packagenum = size//100 + 1
+packagenum = size//1000 + 1
 
-udpCliSock.sendto(str(packagenum).encode(), ADDR)
+windowSize = 5
+windowStart = 0
+windowEnd = windowStart + windowSize
+
+udpCliSock.sendto(str(packagenum).encode(), DISADDR)
 
 for index in range(packagenum):
-    subdata = ""
-    if (index + 1)*100 > size:
-        subdata = data[index*100:]
+    subdata = b''
+    Seq = str(index)
+    while len(Seq) < 8:
+        Seq = '0' + Seq
+    Seq = Seq.encode()
+    if (index + 1)*1000 >= size - 1:
+        subdata = Seq + data[index*1000:]
     else:
-        subdata = data[index*100:(index + 1)*100]
-    udpCliSock.sendto(subdata.encode(), ADDR)
-    # mes, ADDR = udpCliSock.recvfrom(BUFSIZ)
-    # if not mes:
-    #     break
-    # print(mes.decode())
-    print(index/packagenum)
-udpCliSock.sendto(b'EOF', ADDR)
+        subdata = Seq + data[index*1000:(index + 1)*1000]
+    udpCliSock.sendto(subdata, DISADDR)
+    print(Seq.decode(), index, packagenum - 1)
+    ack, addr = udpCliSock.recvfrom(BUFSIZ)
+
+udpCliSock.sendto(b'EOF', DISADDR)
 
 udpCliSock.close()
-
-# while True:
-#     data = input('>')
-#     if not data:
-#         break
-#     udpCliSock.sendto(data.encode(), ADDR)
-#     data, ADDR = udpCliSock.recvfrom(BUFSIZ)
-#     if not data:
-#         break
-#     print(data.decode())
--- Server.py
+++ Server.py
@@ -3,32 +3,38 @@
 from socket import *
 from time import ctime
 import uu
+import pdb
 
 HOST = '127.0.0.1'
 PORT = 21567
-BUFSIZ = 100024
+BUFSIZ = 1024
 ADDR = (HOST, PORT)
+DISADDR = (HOST, 21566)
 
 udpSerSock = socket(AF_INET, SOCK_DGRAM)
 udpSerSock.bind(ADDR)
 print('bind udp on 21567')
-data = b""
+data = b''
 count = 0
-result = ""
+result = b''
 
 packagenum, addr = udpSerSock.recvfrom(BUFSIZ)
 packagenum = int(packagenum.decode())
 
 while True:
     data, addr = udpSerSock.recvfrom(BUFSIZ)
-    if data.decode() == 'EOF':
+    if data == b'EOF':
         break
-    print(count/packagenum)
+    Ack = b'ACK' + data[:9]
+    udpSerSock.sendto(Ack ,DISADDR)
+    print((data[:8]).decode(), count, packagenum)
     count += 1
-    result += data.decode()
+    result += data[8:]
 
-f = open('get.txt', 'w', newline="")
-f.write(result)
-uu.decode('test.txt', 'test.mp4')
+try:
+    f = open('D:/gitRep/LTFP/get.mp4', 'wb')
+    f.write(result)
+except:
+    print("error")
 
 udpSerSock.close()
