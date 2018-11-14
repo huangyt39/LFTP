0596b2dafd5d5c982f2e3682d5c64d9c39a186df
huangyt39 <824551061@qq.com>
2018-11-14 20:35:28 +0800

    flow control

--- Client.py
+++ Client.py
@@ -4,14 +4,17 @@ import time
 from socket import *
 import uu
 import pdb
+import numpy as np
 
 HOST = '127.0.0.1'
 PORT = 21566
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
 DISADDR = (HOST, 21567)
+waitTime = 0.1
 
 udpCliSock = socket(AF_INET, SOCK_DGRAM)
+udpCliSock.settimeout(waitTime)
 udpCliSock.bind(ADDR)
 
 # uu.encode('test.mp4', 'test.txt')
@@ -21,25 +24,42 @@ data = f.read()
 size = len(data)
 packagenum = size//1000 + 1
 
-windowSize = 5
+windowSize = 10
 windowStart = 0
 windowEnd = windowStart + windowSize
+ackFlag = np.array([0]*windowSize)
 
 udpCliSock.sendto(str(packagenum).encode(), DISADDR)
 
-for index in range(packagenum):
-    subdata = b''
-    Seq = str(index)
-    while len(Seq) < 8:
-        Seq = '0' + Seq
-    Seq = Seq.encode()
-    if (index + 1)*1000 >= size - 1:
-        subdata = Seq + data[index*1000:]
-    else:
-        subdata = Seq + data[index*1000:(index + 1)*1000]
-    udpCliSock.sendto(subdata, DISADDR)
-    print(Seq.decode(), index, packagenum - 1)
-    ack, addr = udpCliSock.recvfrom(BUFSIZ)
+while windowStart <= packagenum:
+    if ackFlag.all():
+        break
+    for index in range(windowSize):
+        if ackFlag[index] == 0:
+            subdata = b''
+            Seq = str(windowStart+index)
+            while len(Seq) < 8:
+                Seq = '0' + Seq
+            Seq = Seq.encode()
+            if ((windowStart+index) + 1)*1000 >= size - 1:
+                subdata = Seq + data[(windowStart+index)*1000:]
+            else:
+                subdata = Seq + data[(windowStart+index)*1000:((windowStart+index) + 1)*1000]
+            udpCliSock.sendto(subdata, DISADDR)
+            print(Seq.decode(), packagenum - 1)
+
+    for index in range(windowSize):
+        try:
+            ack, addr = udpCliSock.recvfrom(BUFSIZ)
+        except BaseException as error:
+            print('error in', windowStart + index)
+        else:
+            ackFlag[int(ack[3:])-windowStart] = 1
+    if ackFlag.all():
+        windowStart += windowSize
+        windowEnd += windowSize
+        ackFlag = np.array([0]*windowSize)
+    continue
 
 udpCliSock.sendto(b'EOF', DISADDR)
 
--- Server.py
+++ Server.py
@@ -4,6 +4,7 @@ from socket import *
 from time import ctime
 import uu
 import pdb
+import numpy as np
 
 HOST = '127.0.0.1'
 PORT = 21567
@@ -15,21 +16,44 @@ udpSerSock = socket(AF_INET, SOCK_DGRAM)
 udpSerSock.bind(ADDR)
 print('bind udp on 21567')
 data = b''
-count = 0
 result = b''
+
+windowSize = 10
+windowStart = 0
+windowEnd = windowStart + windowSize
 
 packagenum, addr = udpSerSock.recvfrom(BUFSIZ)
 packagenum = int(packagenum.decode())
+ackFlag = np.zeros(windowSize)
+tmpData = np.array([b'']*windowSize)
 
-while True:
-    data, addr = udpSerSock.recvfrom(BUFSIZ)
-    if data == b'EOF':
-        break
-    Ack = b'ACK' + data[:9]
-    udpSerSock.sendto(Ack ,DISADDR)
-    print((data[:8]).decode(), count, packagenum)
-    count += 1
-    result += data[8:]
+while windowStart <= packagenum:
+    for index in range(windowSize):
+        if ackFlag.all():
+            break
+        data, addr = udpSerSock.recvfrom(BUFSIZ)
+        if data == b'EOF':
+            break
+        Ack = b'ACK' + data[:8]
+        udpSerSock.sendto(Ack ,DISADDR)
+        if ackFlag[index] == 0:
+            ackFlag[index] = 1
+            if index == 0 or ackFlag[:index-1].all():
+                result += data[8:]
+            else:
+                tmpData[index] = data[8:]
+            print((data[:8]).decode(), packagenum)
+    if ackFlag.all():
+        # add package into data
+        for i in range(windowSize):
+            if tmpData[i] != b'':
+                result += tmpData[i][8:]
+        # clean tmp data
+        windowStart += windowSize
+        windowEnd += windowSize
+        ackFlag = np.zeros(windowSize)
+        tmpData = np.array([b'']*windowSize)
+    continue
 
 try:
     f = open('D:/gitRep/LTFP/get.mp4', 'wb')
